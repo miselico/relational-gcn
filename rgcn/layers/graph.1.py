99d99f86f032042a1a7ec8590aff876dda9351cf
michaelcochez <michael.cochez@>
2018-09-06 13:46:32 +0200

    Debug. Two relation types - fixed too high index - check worked. Corrected input now

--- rgcn/layers/graph.1.py
+++ rgcn/layers/graph.1.py
@@ -188,7 +188,7 @@ if __name__ == "__main__":
     number_of_nodes_in_graph = 5
     #adjecancies = [[(1,2)], [], [(2,3), (3,4)]]
     #adjecancies = [[(1,2)], [(1, 2)], [(2,3), (3,4)]]
-    adjecancies = [[(1,2), (2, 3)], [(1, 5)]]
+    adjecancies = [[(1,2), (2, 3)], [(1, 4)]]
     #adjecancies = [[(1,2), (2, 3)]]
 
     input_feature_dim = 11

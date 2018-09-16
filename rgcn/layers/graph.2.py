c6f29f0c9986d62e41176f9c66acb9f9f76557a0
michaelcochez <michael.cochez@>
2018-09-16 20:05:14 +0200

    Trying power of 2 nested stacking + concat - simplest case - correcting reshape

--- rgcn/layers/graph.2.py
+++ rgcn/layers/graph.2.py
@@ -252,7 +252,7 @@ class GraphConvolution(Layer):
 
     def _stackInPairsPow2(self, out_summed, num_elements):
         if num_elements == 1:
-            return out_summed[0]
+            return K.reshape(out_summed[0], (-1, 1, self.output_dim))
         assert num_elements != 0 and ((num_elements & (num_elements - 1)) == 0) # num_elements is a power of 2
         dims = [1]*num_elements
         result = self._stackInPairsPow2Rec(out_summed, dims)
